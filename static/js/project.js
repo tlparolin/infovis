7799b9ab765164247cc559cb97e7767cb6664533
tlparolin <tlparolin@gmail.com>
2023-05-26 10:06:41 -0300

    wip - grafico apareceu

--- static/js/project.js
+++ static/js/project.js
@@ -1,84 +1,94 @@
-/* globals d3, topojson */
+d3.json("https://raw.githubusercontent.com/tlparolin/infovis/master/data/json/dados.json")
+.then(json => {
+    const data = json;
 
-async function start() {
-    const us = await fetch(
-      "https://static.observableusercontent.com/files/6b1776f5a0a0e76e6428805c0074a8f262e3f34b1b50944da27903e014b409958dc29b03a1c9cc331949d6a2a404c19dfd0d9d36d9c32274e6ffbc07c11350ee"
-    ).then(d => d.json());
+    const width = 954;
+    const height = width;
+
+    const innerRadius = Math.min(width, height) * 0.5 - 90;
+    const outerRadius = innerRadius + 10;
+
+    const names = Array.from(new Set(data.flatMap(d => [d.source, d.target]))).sort(d3.ascending)
+
+    const color = d3.scaleOrdinal(names, d3.quantize(d3.interpolateRainbow, names.length));
+
+    const ribbon = d3.ribbonArrow()
+        .radius(innerRadius - 1)
+        .padAngle(1 / innerRadius);
     
-  
-    const radius = d3.scaleSqrt(
-      [0, d3.quantile([...data.values()].sort(d3.ascending), 0.985)],
-      [0, 7]
-    );
-    const format = d3.format(",.0f");
-    const path = d3.geoPath();
-  
-    const chart = await (async () => {
-      const svg = d3.create("svg").attr("viewBox", [0, 0, 975, 610]);
-  
-      svg
-        .append("path")
-        .datum(topojson.feature(us, us.objects.nation))
-        .attr("fill", "#ccc")
-        .attr("d", path);
-  
-      svg
-        .append("path")
-        .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
-        .attr("fill", "none")
-        .attr("stroke", "white")
-        .attr("stroke-linejoin", "round")
-        .attr("d", path);
-  
-      const legend = svg
-        .append("g")
-        .attr("fill", "#777")
-        .attr("transform", "translate(925,608)")
-        .attr("text-anchor", "middle")
-        .style("font", "10px sans-serif")
-        .selectAll("g")
-        .data([1e3, 5e3, 1e4])
-        .join("g");
-  
-      legend
-        .append("circle")
-        .attr("fill", "none")
-        .attr("stroke", "#ccc")
-        .attr("cy", d => -radius(d))
-        .attr("r", radius);
-  
-      legend
-        .append("text")
-        .attr("y", d => -2 * radius(d))
-        .attr("dy", "1.3em")
-        .text(d3.format(".1s"));
-  
-      svg
-        .append("g")
-        .attr("fill", "brown")
-        .attr("fill-opacity", 0.1)
-        .attr("stroke", "#900")
-        .attr("stroke-width", 0.5)
-        .selectAll("circle")
-        .data(
-          topojson
-            .feature(us, us.objects.counties)
-            .features.map(d => ((d.value = data.get(d.id)), d))
-            .sort((a, b) => b.value - a.value)
-        )
-        .join("circle")
-        .attr("transform", d => `translate(${path.centroid(d)})`)
-        .attr("r", d => radius(d.value))
-        .append("title")
-        .text(
-          d => `${d.properties.name}
-    ${format(d.value)}`
-        );
-  
-      return svg.node();
-    })();
+    const arc = d3.arc()
+        .innerRadius(innerRadius)
+        .outerRadius(outerRadius);
+    
+    function matrix(names, data) {
+        const index = new Map(names.map((name, i) => [name, i]));
+        const matrix = Array.from(index, () => new Array(names.length).fill(0));
+        for (const {source, target, value} of data) matrix[index.get(source)][index.get(target)] += value;
+        return matrix;
+    };
 
-    document.querySelector("#chart").appendChild(chart);
-  }
-  
-  start();
+    function _chord(innerRadius){
+        return d3.chordDirected()
+            .padAngle(10 / innerRadius)
+            .sortSubgroups(d3.descending)
+            .sortChords(d3.descending);
+    };
+
+    const matriz = matrix(names, data);
+    const chord = _chord(innerRadius);
+
+    function chart(width, height, matrix, color, names, arc, outerRadius, ribbon) {
+        const svg = d3.create("svg")
+            .attr("viewBox", [-width / 2, -height / 2, width, height]);
+      
+        const chords = chord(matrix);
+        
+        const group = svg.append("g")
+            .attr("font-size", 10)
+            .attr("font-family", "sans-serif")
+            .selectAll("g")
+            .data(chords.groups)
+            .join("g");
+      
+        group.append("path")
+            .attr("fill", d => color(names[d.index]))
+            .attr("d", arc);
+      
+        group.append("text")
+            .each(d => (d.angle = (d.startAngle + d.endAngle) / 2))
+            .attr("dy", "0.35em")
+            .attr("transform", d => `
+                rotate(${(d.angle * 180 / Math.PI - 90)})
+                translate(${outerRadius + 5})
+                ${d.angle > Math.PI ? "rotate(180)" : ""}
+            `)
+            .attr("text-anchor", d => d.angle > Math.PI ? "end" : null)
+            .text(d => names[d.index]);
+      
+        group.append("title")
+            .text(d => `${names[d.index]}
+                ${d3.sum(chords, c => (c.source.index === d.index) * c.source.value)} outgoing →
+                ${d3.sum(chords, c => (c.target.index === d.index) * c.source.value)} incoming ←`);
+      
+        svg.append("g")
+            .attr("fill-opacity", 0.75)
+            .selectAll("path")
+            .data(chords)
+            .join("path")
+            .style("mix-blend-mode", "multiply")
+            .attr("fill", d => color(names[d.target.index]))
+            .attr("d", ribbon)
+            .append("title")
+            .text(d => `${names[d.source.index]} → ${names[d.target.index]} ${d.source.value}`);
+      
+        return svg.node();
+    };
+
+    var grafico = chart(width, height, matriz, color, names, arc, outerRadius, ribbon);
+    var elemento = document.getElementById("chart");
+    elemento.append(grafico);
+    
+})
+.catch(error => {
+    console.error(error);
+});
