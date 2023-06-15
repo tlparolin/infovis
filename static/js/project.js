cdbd99f6b7188a16f48eb88642b237283b007378
tlparolin <tlparolin@gmail.com>
2023-06-15 16:48:27 -0300

    Adicionado tooltips aos botões

--- static/js/project.js
+++ static/js/project.js
@@ -1,5 +1,9 @@
 $( document ).ready(function() {
     resumo();
+
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
 });
 
 function tutorial(){
